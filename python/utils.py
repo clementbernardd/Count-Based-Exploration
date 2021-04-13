96a66648099ca72fd09dd34183ce744cfdd347fa
clementbernard <clementbernardd@gmail.com>
2021-04-13 23:17:01 +0200

    update utils

--- python/utils.py
+++ python/utils.py
@@ -10,3 +10,24 @@ def running_mean(x, N):
     result = np.convolve(x,mask,'same')
 
     return result
+
+
+def plot_count(q_learning, q_learning_count, figsize = (14,8), window = 10, name = 'Q_learning') :
+
+  fig, ax = plt.subplots(figsize = figsize, nrows = 1, ncols = 1)
+  ax = np.array(ax)
+  colors = ['g','orange']
+  labels = ['Baseline', 'Count-based']
+  models = [q_learning , q_learning_count]
+
+  for i,label in enumerate(labels) :
+    ax.flatten()[0].plot(models[i].plotting['Rewards'], alpha = 0.2, color =colors[i])
+    ax.flatten()[0].plot(running_mean(models[i].plotting['Rewards'], window),color =colors[i], alpha=1, label = label)
+
+    ax.flatten()[0].set_xlabel('Episode')
+    ax.flatten()[0].set_ylabel('Rewards')
+    ax.flatten()[0].grid(True)
+    ax.flatten()[0].set_title(name)
+    ax.flatten()[0].legend()
+
+  return None
